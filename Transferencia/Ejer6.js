9db83e2684b7e6b7999e36d9332b4b9b4b03be6e
XimenaRubio212 <ximenarubio121@gmail.com>
2025-11-10 14:49:20 -0500

    Se corrigio el punto 6 de Transferencia donde el usuario debe adivinar el numero aleatorio

--- Transferencia/Ejer6.js
+++ Transferencia/Ejer6.js
@@ -7,6 +7,6 @@ do {
     //se solicita al usuario que adivine el número
   intento = Number(prompt("Adivina el número (1-20):"));
   //se muestra si el intento es mayor, menor o correcto
-  console.log(intento < numero ? "Mayor" : intento > numero ? "Menor" : "¡Correcto!");
+  alert(intento < numero ? "Mayor" : intento > numero ? "Menor" : "¡Correcto!");
   //se convierte la respuesta a minúsculas
 } while (intento !== numero);
