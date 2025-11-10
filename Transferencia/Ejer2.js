07d41c54022b45027894bf00c264a4f3ef9c6a84
XimenaRubio212 <ximenarubio121@gmail.com>
2025-11-10 14:52:48 -0500

    Se corrigio el punto 2 de Transferencia donde el usuario solo le indicaba a la maquina un numero para realizar una multiplicacion hasta 12 lo que se corrio al igual que el punto 6 fue solo cambiar por consola a un mensaje de alerta

--- Transferencia/Ejer2.js
+++ Transferencia/Ejer2.js
@@ -5,8 +5,9 @@ do {
   //se inicializa la variable n y se solicita al usuario que ingrese un número
   let n = Number(prompt("Ingrese un número:"));
   //se crea un ciclo for y se muestra la tabla de multiplicar del número ingresado
-  for (let i = 1; i <= 12; i++) console.log(`${n} x ${i} = ${n*i}`);
+  for (let i = 1; i <= 12; i++)
+    alert(`${n} x ${i} = ${n*i}`);
   //se solicita al usuario si desea otra tabla
-  otra = prompt("¿Otra tabla? (si/no)");
+    otra = prompt("¿Otra tabla? (si/no)");
   //se convierte la respuesta a minúsculas
 } while (otra === "si");
